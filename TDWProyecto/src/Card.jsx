10512583ec1c9de576c245a8257145ad39f44a80
Miguelkoro <miguelkoro@gmail.com>
2025-04-02 14:02:36 +0200

    Creada la estructura general con las secciones y las cards

--- TDWProyecto/src/Card.jsx
+++ TDWProyecto/src/Card.jsx
@@ -4,9 +4,9 @@ import './Card.css'; // Archivo CSS para estilos
 const Card = () => {
   return (
     <div className="card">
-      <img className="card-image" src="./assets/images/CSS.png" alt="Imagen Ejemplo"></img>
-      <h2 className='card-title' >Ejemplo Titulo</h2>
-      <p className='card-description'>Descripcion sobre la entidad, persona o empresa del ejemplo</p>
+      <img className="card-image" src="./assets/images/HTML5.png" alt="Imagen Ejemplo"></img>
+      <h2 className='card-title' >Ejemplo  Tithhdffuddo de fhfhff jsjdds</h2>
+      <p className='card-description'>Descrijjddddpcion sobre la entidad, persona o empresa del ejemplo</p>
     </div>
   );
 }
--- TDWProyecto/src/Section.jsx
+++ TDWProyecto/src/Section.jsx
@@ -3,12 +3,26 @@ import './Section.css'; // Archivo CSS para estilos
 import Card from './Card'; // Importa el componente de sección pequeña
 
 const Section = () => {
-  return (   
-    <>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-    </> 
+  return (
+    <div className="section-container">
+      <h1 className="section-title">Ejemplo de Sección</h1>
+      <div className="card-wrapper">
+        <Card />
+        <Card />
+
+        <Card />
+        <Card />
+        <Card />
+
+        <Card />
+        <Card />
+        <Card />
+
+        <Card />
+      </div>
+      <hr className='section-hr'/>
+    </div>
+    
   );
 };
 
