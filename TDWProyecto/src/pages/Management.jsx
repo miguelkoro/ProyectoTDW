22052043fd3f4e01ee28dfd588b8ea77239b7f0b
Miguelkoro <miguelkoro@gmail.com>
2025-05-15 23:16:33 +0200

    Bug: en management al actualizar no salia el error de no hay datos disponibles

--- TDWProyecto/src/pages/Management.jsx
+++ TDWProyecto/src/pages/Management.jsx
@@ -47,7 +47,7 @@ const Management = (props) => {
     if (!isLoading) {
       const timer = setTimeout(() => {
         setShowNoData(true); // Activa el estado después del retraso
-      }, 500); // Retraso de 300 ms
+      }, 300); // Retraso de 300 ms
 
       return () => clearTimeout(timer); // Limpia el temporizador si el componente se desmonta o `isLoading` cambia
     } else {
@@ -55,7 +55,7 @@ const Management = (props) => {
     }
     console.log("isLoading", isLoading); // Muestra el estado de carga en la consola
 
-  }, [props.objects]);
+  }, [objects]);
 
   // Observa los cambios en las variables del contexto y actualiza el estado `objects`
   useEffect(() => {
